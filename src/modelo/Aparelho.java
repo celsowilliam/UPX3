014900649a6033e94f85b259450f2898ca769948
Celso William <celsowilliam299@gmail.com>
2026-04-19 01:50:34 -0300

    Corrigindo metods Aparelho, Consumo, Simulação e implementando inicio da Main!

--- src/modelo/Aparelho.java
+++ src/modelo/Aparelho.java
@@ -1,4 +1,7 @@
-package src.modelo;
+package modelo;
+import modelo.Consumo;
+
+// ALINHADO COM BANCO DE DADOS DO CELSO KKKK 19/04
 
 public class Aparelho {
     private String nome;
@@ -6,6 +9,7 @@ public class Aparelho {
     private int quantidade;
     private double horasUsoDiario;
     private int diasUsoMes;
+    private int mes;
 
     public Aparelho(String nome, double potenciaWatts, int quantidade, double horasUsoDiario, int diasUsoMes) {
         this.nome = nome;
@@ -13,12 +17,13 @@ public class Aparelho {
         this.quantidade = quantidade;
         this.horasUsoDiario = horasUsoDiario;
         this.diasUsoMes = diasUsoMes;
+        this.mes = mes;
     }
 
     public double getConsumoKwhMes() {
         // Cálculo: (Watts * Horas * Dias * Qtd) / 1000
         return (potenciaWatts * horasUsoDiario * diasUsoMes * quantidade) / 1000;
-    }
+    } // OK BANCO
 
     public double getCustoIndividual(double precoKwh) {
         return getConsumoKwhMes() * precoKwh;
@@ -27,4 +32,5 @@ public class Aparelho {
     public String getNome() { return nome; }
     public double getPotenciaWatts() { return potenciaWatts; }
     public int getQuantidade() { return quantidade; }
+    public int getMes() { return mes; }
 }
